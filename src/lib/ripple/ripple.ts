a94ff1ee6faffeb2bc1a2f8771d4660aa2411ae5
HaRuki Kuriwada <haruki.kuriwada@hennge.com>
2024-06-29 02:10:23 +0900

    ripple effect migration

--- src/lib/ripple/ripple.ts
+++ src/lib/ripple/ripple.ts
@@ -1,38 +1,38 @@
 // ref: https://svelte.dev/repl/61d9178d2b9944f2aa2bfe31612ab09f?version=3.6.7
-function onRipple(color: string, centered?: boolean): (event: MouseEvent) => void {
-	return (event: MouseEvent) => {
+function getRippleEffect(bgColor: string, centered?: boolean): (event: MouseEvent) => void {
+	return function (event: MouseEvent) {
 		const target = event.currentTarget as HTMLElement;
+		target.style.overflow = 'hidden'; // make sure the ripple effect is not going outside the parent
+
 		const circle = document.createElement('span');
-		const d = Math.max(target.clientWidth, target.clientHeight);
+		const diameter = Math.max(target.clientWidth, target.clientHeight); // diameter of the circle
 
 		const removeCircle = () => {
-			console.log('remove circle');
-			circle.remove();
 			circle.removeEventListener('animationend', removeCircle);
+			circle.remove();
 		};
 
 		circle.addEventListener('animationend', removeCircle);
-		circle.style.width = circle.style.height = `${d}px`;
+		circle.style.width = circle.style.height = diameter + 'px';
+		circle.style.backgroundColor = bgColor;
 		const rect = target.getBoundingClientRect();
 
 		if (centered) {
-			circle.classList.add('absolute', 'top-0', 'left-0', 'ripple-centered', `bg-${color}-200`);
+			circle.classList.add('top-0', 'left-0');
 		} else {
-			circle.style.left = `${event.clientX - rect.left - d / 2}px`;
-			circle.style.top = `${event.clientY - rect.top - d / 2}px`;
-
-			circle.classList.add('ripple-normal', `bg-${color}-200`);
+			circle.style.left = event.clientX - rect.left - diameter / 2 + 'px';
+			circle.style.top = event.clientY - rect.top - diameter / 2 + 'px';
 		}
 
-		circle.classList.add('ripple');
+		circle.classList.add('absolute', 'rounded-full', 'animate-[ripple_1.0s]');
 
 		target.appendChild(circle);
 	};
 }
 
-export default function useRipple(color = 'primary', centered = false) {
+export function onRipple(bgColor = 'white', centered = false) {
 	return function (node: HTMLElement) {
-		const onMouseDown = onRipple(color, centered);
+		const onMouseDown = getRippleEffect(bgColor, centered);
 		node.addEventListener('mousedown', onMouseDown);
 
 		return {
--- src/stories/modal/examples/ModalTemplate.svelte
+++ src/stories/modal/examples/ModalTemplate.svelte
@@ -27,7 +27,9 @@
 <Modal {size} {title} {dismissible} bind:open={popupModal}>
 	<div class="text-center">
 		<h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">This is Hello world!</h3>
-		<Button color="red" class="me-2">Yes</Button>
-		<Button color="alternative">No</Button>
+		<div class="flex flex-row justify-center gap">
+			<Button color="red" class="me-2">Yes</Button>
+			<Button color="alternative">No</Button>
+		</div>
 	</div>
 </Modal>

5b831944563829ca6c5d4704d92232e3246e36b0
Phoebe <phyuhala@gmail.com>
2024-07-27 13:39:44 -0700

    Tooltip unit test

--- src/lib/tooltip/__test__/Tooltip.test.ts
+++ src/lib/tooltip/__test__/Tooltip.test.ts
@@ -13,9 +13,68 @@ describe('Tooltip component', async () => {
 	test('should render tooltip', async () => {
 		render(TooltipTest);
 		const trigger = screen.getByRole('button');
-
 		await userEvent.hover(trigger);
 		expect(screen.getByRole('tooltip')).toBeTruthy();
+	});
+
+	//title
+	test('should show the correct title while a user hovers the tooltip', async () => {
+		render(Tooltip, { title: 'This is title' });
+		const trigger = screen.getByRole('button');
+		await userEvent.hover(trigger);
+		expect(screen.getByText('This is title')).toBeTruthy();
+	});
+
+	//size
+	test('should show the correct size of the title while a user hovers the tooltip', async () => {
+		render(Tooltip, { size: 'sm' });
+		const trigger = screen.getByRole('button');
+		await userEvent.hover(trigger);
+		expect(screen.getByRole('tooltip').classList.contains('text-sm')).toBeTruthy();
+	});
+	test('should show the correct size of the title while a user hovers the tooltip', async () => {
+		render(Tooltip, { size: 'md' });
+		const trigger = screen.getByRole('button');
+		await userEvent.hover(trigger);
+		expect(screen.getByRole('tooltip').classList.contains('text-base')).toBeTruthy();
+	});
+	test('should show the correct size of the title while a user hovers the tooltip', async () => {
+		render(Tooltip, { size: 'lg' });
+		const trigger = screen.getByRole('button');
+		await userEvent.hover(trigger);
+		expect(screen.getByRole('tooltip').classList.contains('text-lg')).toBeTruthy();
+	});
+
+	//variant
+	test('should show the correct variant of the title while a user hovers the tooltip', async () => {
+		render(Tooltip, { variant: 'primary' });
+		const trigger = screen.getByRole('button');
+		await userEvent.hover(trigger);
+		expect(screen.getByRole('tooltip').classList.contains('bg-blue-500')).toBeTruthy();
+	});
+	test('should show the correct variant of the title while a user hovers the tooltip', async () => {
+		render(Tooltip, { variant: 'secondary' });
+		const trigger = screen.getByRole('button');
+		await userEvent.hover(trigger);
+		expect(screen.getByRole('tooltip').classList.contains('bg-neutral-500')).toBeTruthy();
+	});
+	test('should show the correct variant of the title while a user hovers the tooltip', async () => {
+		render(Tooltip, { variant: 'success' });
+		const trigger = screen.getByRole('button');
+		await userEvent.hover(trigger);
+		expect(screen.getByRole('tooltip').classList.contains('bg-green-500')).toBeTruthy();
+	});
+	test('should show the correct variant of the title while a user hovers the tooltip', async () => {
+		render(Tooltip, { variant: 'warning' });
+		const trigger = screen.getByRole('button');
+		await userEvent.hover(trigger);
+		expect(screen.getByRole('tooltip').classList.contains('bg-yellow-500')).toBeTruthy();
+	});
+	test('should show the correct variant of the title while a user hovers the tooltip', async () => {
+		render(Tooltip, { variant: 'danger' });
+		const trigger = screen.getByRole('button');
+		await userEvent.hover(trigger);
+		expect(screen.getByRole('tooltip').classList.contains('bg-red-500')).toBeTruthy();
 	});
 
 	// TODO: Add tests for Tooltip component
--- src/lib/tooltip/__test__/examples/TooltipTest.svelte
+++ src/lib/tooltip/__test__/examples/TooltipTest.svelte
@@ -1,7 +1,5 @@
 <script>
 	import Tooltip from '$lib/tooltip/Tooltip.svelte';
-
-  
 </script>
 
 <Tooltip title="Tooltip">Hover me</Tooltip>

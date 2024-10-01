af553480d4c06c01a4b12d016a007a33bb19cb0a
Phoebe <phyuhala@gmail.com>
2024-09-30 21:23:10 -0700

    shrink the  nav logo

--- src/routes/global-components/Header.svelte
+++ src/routes/global-components/Header.svelte
@@ -38,7 +38,7 @@
 	<nav class="flex flex-row items-center justify-between py-2 px-8">
 		<div class="flex flex-row gap-4">
 			<Link href="/">
-				<Logo fill_1="#231f20" fill_2="#427cbf" fill_3="#2e3192" size={140}></Logo>
+				<Logo fill_1="#231f20" fill_2="#427cbf" fill_3="#2e3192" size={100}></Logo>
 			</Link>
 			<!-- TODO: Let's automate... i.e. GitHub API? release number fetching?? -->
 			<span>ver. 0.0.1</span>
